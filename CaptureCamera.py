128584690258727dcb9ff7fe2bbceb27265c2c51
SarifTech <sariftrainning@gmail.com>
2015-08-15 12:55:45 -0300

    Add important notes.

--- CaptureCamera.py
+++ CaptureCamera.py
@@ -1,24 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-OpenCV Simple Capturing camera frames
+OpenCV Simple Capturing camera frames and store a 10 sec video
 Created on Sat Aug 15 12:00:30 2015
 
 @author: Bruno Godoi Eilliar
 """
 
+# Import necessary libraries
 import cv2
 
+# Change it to store on your local machine
+VideoDirectory = '/home/bruno/Videos/'
+
+# Objecto to capture image, default webcam on Ubuntu is video0
 cameraCapture = cv2.VideoCapture(0)
 fps = 30 #assumption
 
-# Size of the image (autocmatically)
+# Size of the image (automatically)
 size = (int(cameraCapture.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)),
         int(cameraCapture.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)))
 
-videoWriter = cv2.VideoWriter('/home/bruno/Videos/MyOutputVid.avi',
+# Will create a .avi file to store the 10 sec video
+videoWriter = cv2.VideoWriter(VideoDirectory+'MyOutputVid.avi',
                               cv2.cv.CV_FOURCC('I', '4', '2', '0'),
                               fps, size)
-                              
+
+# Read a frame from the camera
 sucess, frame = cameraCapture.read()
 numFramesRemaining = 10*fps -1
 
