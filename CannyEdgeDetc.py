7407fd569dda40c6d2e2ba83ec74aa9534f9d595
Bruno G. Eilliar <lord.eilliar@hotmail.com>
2015-08-28 15:25:02 -0300

    Add Canny Edges Detection Example.

--- CannyEdgeDetc.py
+++ CannyEdgeDetc.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Smooth Filter Example
-Created on Tue Aug 25 14:46:25 2015
+Canny Edge Detection
+Created on Fri Aug 28 15:15:48 2015
 
 @author: Bruno Godoi Eilliar
 Notes:
-http://docs.opencv.org/modules/imgproc/doc/filtering.html#smooth
-
+http://docs.opencv.org/modules/imgproc/doc/feature_detection.html?highlight=canny#canny
 """
-import numpy as np
-debug= True
+debug = True
 import cv2
+import numpy as np
 
 # Object to capture image, default webcam on Ubuntu is video0
 cameraCapture = cv2.VideoCapture(0)
@@ -20,20 +19,21 @@ cameraCapture = cv2.VideoCapture(0)
 if debug: print "Frame size: ", (w,h)
 # Open a Window
 cv2.namedWindow("Video")
-cv2.namedWindow("Filtered Video")
+cv2.namedWindow("Canny Edges")
+
 print "Starting Video Streaming. Press any key to stop."
 success, frame = cameraCapture.read()
-s_frame = frame.copy()
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 if debug: print "Frame Shape: ", np.shape(frame)
 
 while success and cv2.waitKey(1) == -1:
     # Take a frame
     success, frame = cameraCapture.read()
-    # Bluring the image
-    s_frame = cv2.blur(frame, (5,5))
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray,100,200)
     # Show Picture
     cv2.imshow("Video", frame)
-    cv2.imshow("Filtered Video", s_frame)
+    cv2.imshow("Canny Edges", edges)
     
 cameraCapture.release()
 cv2.destroyAllWindows()
